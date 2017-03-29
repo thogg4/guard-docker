56a25286f19063caeccbd1d911b3dea9cf01efb1
Tim Hogg <thogg4@gmail.com>
2017-03-29 11:27:51 -0600

    init

---
+++ guard-docker.gemspec
@@ -0,0 +1,36 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+Gem::Specification.new do |spec|
+  spec.name          = "guard-docker"
+  spec.version       = '0.0.1'
+  spec.authors       = ["Tim Hogg"]
+  spec.email         = ["thogg4@gmail.com"]
+
+  spec.summary       = %q{Write a short summary, because Rubygems requires one.}
+  spec.description   = %q{Write a longer description or delete this line.}
+  spec.homepage      = "http://github.com"
+  spec.license       = "MIT"
+
+  # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
+  # to allow pushing to a single host or delete this section to allow pushing to any host.
+  if spec.respond_to?(:metadata)
+    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
+  else
+    raise "RubyGems 2.0 or newer is required to protect against " \
+      "public gem pushes."
+  end
+
+  spec.files         = `git ls-files -z`.split("\x0").reject do |f|
+    f.match(%r{^(test|spec|features)/})
+  end
+  spec.bindir        = "exe"
+  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.require_paths = ["lib"]
+
+  spec.add_dependency 'guard', '>= 1.1'
+
+  spec.add_development_dependency "bundler", "~> 1.13"
+  spec.add_development_dependency "rake", "~> 10.0"
+end
---
+++ lib/guard/docker.rb
@@ -0,0 +1,117 @@
+require 'guard'
+
+module Guard
+  class Docker < Plugin
+
+    # Initializes a Guard plugin.
+    # Don't do any work here, especially as Guard plugins get initialized even if they are not in an active group!
+    #
+    # @param [Hash] options the custom Guard plugin options
+    # @option options [Array<Guard::Watcher>] watchers the Guard plugin file watchers
+    # @option options [Symbol] group the group this Guard plugin belongs to
+    # @option options [Boolean] any_return allow any object to be returned from a watcher
+    #
+    def initialize(options = {})
+      
+      @image = options.fetch(:image)
+      @host_port = options.fetch(:host_port)
+      @container_port = options.fetch(:container_port)
+      @env_vars = options.fetch(:env_vars)
+
+      super
+    end
+
+    # Called once when Guard starts. Please override initialize method to init stuff.
+    #
+    # @raise [:task_has_failed] when start has failed
+    # @return [Object] the task result
+    #
+    def start
+      cmd = []
+      cmd << 'docker run --rm'
+      cmd << "-p #{@host_port}:#{@container_port}"
+      @env_vars.each do |key, value|
+        cmd << "-e #{key}=#{value}"
+      end
+      cmd << "--name=guard-#{@image}"
+      cmd << @image
+
+      spawn(cmd.join(' '))
+
+      success "#{@image} is running"
+    end
+
+    # Called when `stop|quit|exit|s|q|e + enter` is pressed (when Guard quits).
+    #
+    # @raise [:task_has_failed] when stop has failed
+    # @return [Object] the task result
+    #
+    def stop
+      cmd = []
+      cmd << "docker stop guard-#{@image}"
+      system(cmd.join(' '))
+    end
+
+    # Called when `reload|r|z + enter` is pressed.
+    # This method should be mainly used for "reload" (really!) actions like reloading passenger/spork/bundler/...
+    #
+    # @raise [:task_has_failed] when reload has failed
+    # @return [Object] the task result
+    #
+    def reload
+    end
+
+    # Called when just `enter` is pressed
+    # This method should be principally used for long action like running all specs/tests/...
+    #
+    # @raise [:task_has_failed] when run_all has failed
+    # @return [Object] the task result
+    #
+    def run_all
+    end
+
+    # Called on file(s) additions that the Guard plugin watches.
+    #
+    # @param [Array<String>] paths the changes files or paths
+    # @raise [:task_has_failed] when run_on_additions has failed
+    # @return [Object] the task result
+    #
+    def run_on_additions(paths)
+    end
+
+    # Called on file(s) modifications that the Guard plugin watches.
+    #
+    # @param [Array<String>] paths the changes files or paths
+    # @raise [:task_has_failed] when run_on_modifications has failed
+    # @return [Object] the task result
+    #
+    def run_on_modifications(paths)
+    end
+
+    # Called on file(s) removals that the Guard plugin watches.
+    #
+    # @param [Array<String>] paths the changes files or paths
+    # @raise [:task_has_failed] when run_on_removals has failed
+    # @return [Object] the task result
+    #
+    def run_on_removals(paths)
+    end
+
+    def pending message
+      notify message, :image => :pending
+    end
+
+    def success message
+      notify message, :image => :success
+    end
+
+    def failed message
+      notify message, :image => :failed
+    end
+
+    def notify(message, options = {})
+      Notifier.notify(message, options)
+    end
+
+  end
+end
